109b7ba4b4cc2b40f1db37b4a1ff58b8b087692a
colorBeige <133912252+colorBeige@users.noreply.github.com>
2024-12-03 15:10:12 -0500

    additional info

--- server/init.js
+++ server/init.js
@@ -8,3 +8,12 @@
 ** but you cannot just copy and paste it--you script has to do more to handle
 ** users.
 */
+
+/*
+From Docs
+Include a script called server/init.js in your repository that will be run to create initial data in
+your schema. This script will be run using Node before starting the server. The initial data must
+contain the user profile for admin. The email address, display name, and password for an admin
+user must be provided as the command-line arguments to server/init.js. You must use these
+credentials to create a user profile for admin in the database.
+*/
